db768eba7dfe2f382900cac3bf294906ed65730d
tornaizsolt99 <zsoltika991001@gmail.com>
2020-12-09 23:50:07 +0200

    added favorites fragment and recyclerviews

--- app/src/main/java/com/example/where_to_eat/MainActivity.kt
+++ app/src/main/java/com/example/where_to_eat/MainActivity.kt
@@ -3,23 +3,30 @@ package com.example.where_to_eat
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import butterknife.BindView
+import butterknife.ButterKnife
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
+    @BindView(R.id.main_bottom_navigation) lateinit var navi: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        ButterKnife.bind(this)
 
         var profileFragment= ProfileFragment()
         var restaurantFragment= RestaurantFragment()
+        var favoritesFragment= FavoritesFragment()
+
 
         var bottomNavigationView=findViewById<BottomNavigationView>(R.id.main_bottom_navigation)
-        bottomNavigationView.setOnNavigationItemSelectedListener {
+        navi.setOnNavigationItemSelectedListener {
 
             when(it.itemId){
                 R.id.menu_item_profile -> replacefragment(profileFragment)
                 R.id.menu_item_restaurant ->replacefragment(restaurantFragment)
+                R.id.menu_item_favorites -> replacefragment(favoritesFragment)
             }
             true
 
