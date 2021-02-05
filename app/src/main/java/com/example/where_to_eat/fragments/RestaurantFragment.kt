623a71887263ff26416082ce845c434d6b0d344f
tornaizsolt99 <zsoltika991001@gmail.com>
2021-02-05 03:23:59 +0200

    details fragment implemented

--- app/src/main/java/com/example/where_to_eat/fragments/RestaurantFragment.kt
+++ app/src/main/java/com/example/where_to_eat/fragments/RestaurantFragment.kt
@@ -7,8 +7,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.where_to_eat.Restaurants
 import com.example.where_to_eat.RestaurantsAdapter
 import com.example.where_to_eat.RestaurantsList
+import com.example.where_to_eat.api.ApiClient
 import com.example.where_to_eat.databinding.FragmentRestaurantBinding
 import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
@@ -17,10 +19,6 @@ import kotlinx.coroutines.launch
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -28,16 +26,11 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class RestaurantFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
     private var _binding: FragmentRestaurantBinding? = null
     private val binding get() = _binding!!
@@ -52,17 +45,8 @@ class RestaurantFragment : Fragment() {
         val request= Request.Builder().url("https://ratpark-api.imok.space/restaurants?country=US&page=1").build()
         CoroutineScope(Dispatchers.IO).launch {
             try{
-                val response =clientt.newCall(request).execute()
-                val gson=Gson()
-                 val apiResponse: RestaurantsList =gson.fromJson(response.body()?.charStream(), RestaurantsList::class.java)
-                 Log.d("zsolt", "oksii ")
-                Log.d("zsolt","naa ${apiResponse.per_page} ${apiResponse.total_entries} ${apiResponse.restaurants[1].name}")
-               // binding.textView.text= apiResponse.restaurant[0].name
-                activity?.runOnUiThread {
-                    binding.restaurantRecyclerview.adapter = RestaurantsAdapter(apiResponse.restaurants)
-                    binding.restaurantRecyclerview.layoutManager = LinearLayoutManager(context)
-                    binding.restaurantRecyclerview.setHasFixedSize(true)
-                }
+                val response= ApiClient.getInstance().fetchAllRestaurants().execute().body()
+                setRestautants(response!!.restaurants)
             }
             catch (e:Exception){
 
@@ -71,49 +55,22 @@ class RestaurantFragment : Fragment() {
             }
         }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
         return view
-
-       // return inflater.inflate(R.layout.fragment_restaurant, container, false)
     }
+
+    fun setRestautants(restaurants: List<Restaurants>){
+        activity?.runOnUiThread {
+            binding.restaurantRecyclerview.adapter = RestaurantsAdapter(restaurants)
+            binding.restaurantRecyclerview.layoutManager = LinearLayoutManager(context)
+            binding.restaurantRecyclerview.setHasFixedSize(true)
+
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment RestaurantFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-                RestaurantFragment().apply {
-                    arguments = Bundle().apply {
-                        putString(ARG_PARAM1, param1)
-                        putString(ARG_PARAM2, param2)
-                    }
-                }
-    }
+
 }
